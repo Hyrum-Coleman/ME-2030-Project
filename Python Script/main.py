edffd3ec356e2739f5db544bb82439fedba1ce1d
Hyrum Coleman <99374726+Hyrum-Coleman@users.noreply.github.com>
2022-12-08 10:09:53 -0700

    added bisection troll

--- Python Script/main.py
+++ Python Script/main.py
@@ -25,16 +25,48 @@ import math
 MASS = 0.028  # mass of the ball in kg
 RADIUS = 1.0  # radius of the loop in m
 RAMP_ANGLE = 30.0  # angle of the ramp in degrees
+theta_rad = RAMP_ANGLE * math.pi / 180
+theta_ball = 47.795
+theta_ball_rad = theta_ball * math.pi / 180
 g = 9.8  # gravitational acceleration in m/s^2
 rho = 1.225  # density of the air in kg/m^3
 Cd = 0.47  # drag coefficient of a sphere
+r_ball = 0.0159  # radius of the ball in m
+mu_s = .55
 
 
-def ball_motion(min_height, max_height, tolerance, dt):
-    
+def bisection(f, a, b, tol):
+    while abs(a - b) > tol:
+        c = (a + b) / 2
+        if f(c) == 0:
+            return c
+        elif f(a) * f(c) < 0:
+            b = c
+        else:
+            a = c
+    return (a + b) / 2
+
+
+def f(h):
+    N = (MASS * g * math.cos(theta_rad)) / (2 * math.cos(theta_ball_rad))
+    term1 = MASS * g * h
+    term2 = (mu_s * N * (h - RADIUS * (1 - math.cos(theta_rad))) / (math.sin(theta_rad))) * (RADIUS * (RAMP_ANGLE + 180))
+    term3 = -MASS * g * 2 * RADIUS
+    term4 = -( MASS * RADIUS * g) / 2
+    term5 = (g * MASS * r_ball ** 2) / 5
+    VALUE = term1 + term2 + term3 + term4 + term5
+    return VALUE
+
 
 def main():
-    gaming = 0
+    a = .001
+    b = 100
+    tol = .001
+
+    min_drop_height = bisection(f, a, b, tol)
+
+    print(f"The minimum drop height is {min_drop_height} m.")
+    print(f"The minimum drop height is {min_drop_height * 39.3701} in.")
 
 
 if __name__ == '__main__':
