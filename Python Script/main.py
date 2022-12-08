b1aea1cfc00adee195bf772d700cd874d0b51985
Hyrum Coleman <99374726+Hyrum-Coleman@users.noreply.github.com>
2022-12-08 09:33:23 -0700

    added block comment at the top of the script

--- Python Script/main.py
+++ Python Script/main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+# make a header comment for me to type in
+""""
+This is a script for part 2 of the fall 2022 dynamics project.
+It will be used to calculate the minimum drop height for a ball to complete a loop on a ramp.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+The true ramp angle is unknown, so we will just use 30 degrees for now.
+The true radius of the loop is unknown, so we will just use 1 meter for now.
+The mass of the ball is .028 kg
+The radius of the ball .0159 m
+The density of the air is 1.225 kg/m^3
+The drag coefficient of a sphere is 0.47
+The ball is rolling in between two rails, .011 m apart
+
+We will use the bisection method to find the minimum drop height.
+We will use a tolerance of 0.001 m.
+
+We will use a timestep of 0.001 s.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import math
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def main():
+    gaming = 0
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
